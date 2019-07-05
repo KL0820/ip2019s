8077373583bc04fb83e9a95a33e80aa7225be868
KL0820 <52515385+KL0820@users.noreply.github.com>
2019-07-05 17:43:12 +0800

    Add files via upload

--- portfolio/labs/lab01/lab.js
+++ portfolio/labs/lab01/lab.js
@@ -4,6 +4,40 @@
 
 
 function start(e) {
+    var name = [
+        {
+            char: '李',
+            big5: 'A7F5',
+            cns: '1-4A57',
+            unicode: '674E'
+        },
+        {
+            char: '以',
+            big5: 'A548',
+            cns: '1-4568',
+            unicode: '4EE5'
+        },
+        {
+            char: '謙',
+            big5: 'C1BE',
+            cns: '1-7549',
+            unicode: '8B19'
+        },
+        ];
+
+    var ele = document.getElementById("div1");
+
+    for(var i = 0;i < 3;i++){
+        ele.innerHTML += name[i].char;
+        ele.innerHTML += ' big5 = ';
+        ele.innerHTML += name[i].big5;
+        ele.innerHTML += ' cns = ';
+        ele.innerHTML += name[i].cns;
+        ele.innerHTML += '  unicode = ';
+        ele.innerHTML += name[i].unicode;
+        ele.innerHTML += '<br>';
+        }
+
 
 }
 
